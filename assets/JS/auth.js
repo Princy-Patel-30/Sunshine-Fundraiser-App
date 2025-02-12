7f12aef6be2ae50532004336247935e79080fe51
adityamakadiya <aditya.makadiya@bacancy.com>
2025-02-12 12:19:04 +0530

    Signup Commit

---
+++ assets/JS/auth.js
@@ -0,0 +1,73 @@
+function toggleForm() {
+    let loginForm = document.getElementById("login-form");
+    let signupForm = document.getElementById("signup-form");
+    
+    if (loginForm.style.display === "none") {
+        loginForm.style.display = "block";
+        signupForm.style.display = "none";
+    } else {
+        loginForm.style.display = "none";
+        signupForm.style.display = "block";
+    }
+}
+
+function getNextUserId() {
+    let lastUserId = localStorage.getItem("lastUserId") || 0;
+    let newUserId = parseInt(lastUserId) + 1;
+    localStorage.setItem("lastUserId", newUserId); // Save updated ID
+    return newUserId;
+}
+
+function getNextNgoId() {
+    let lastNgoId = localStorage.getItem("lastNgoId") || 100;
+    let newNgoId = parseInt(lastNgoId) + 1;
+    localStorage.setItem("lastNgoId", newNgoId); // Save updated ID
+    return newNgoId;
+}
+
+
+
+function registerUser() {
+    let username = document.getElementById("signup-username").value;
+    let password = document.getElementById("signup-password").value;
+    let email = document.getElementById("email").value;
+    let role = document.getElementById("signup-role").value;
+
+    if (role === "User") {
+        let users = JSON.parse(localStorage.getItem("users")) || [];
+        let userId = getNextUserId();
+
+        let newUser = {
+            id: userId,
+            name: username,
+            email: email,
+            password: password
+        };
+
+        users.push(newUser);
+        localStorage.setItem("users", JSON.stringify(users));
+        alert("User registered successfully with User ID: " + userId);
+        
+    } else if (role === "NGO") {
+        let NGOs = JSON.parse(localStorage.getItem("NGO")) || []; // Changed variable name to be more clear
+        let ngoId = getNextNgoId();
+
+        let newNgo = {
+            id: ngoId,
+            name: username,
+            email: email,
+            password: password
+        };
+
+        NGOs.push(newNgo);
+        localStorage.setItem("NGO", JSON.stringify(NGOs));
+        alert("NGO registered successfully with NGO ID: " + ngoId);
+        //window.location.href = "pages\login.html";
+    } else {
+        console.log("Invalid role selected:", role);
+    }
+    
+    
+}
+
+
